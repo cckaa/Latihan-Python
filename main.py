cae968e3b0d7a760bd287cf37210ec6be39788f6
cckaa <92702411+cckaa@users.noreply.github.com>
2021-10-24 02:17:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+#Biodata Mahasiswa
+
+print("#### Tugas Pemograman Python ####")
+print("Nama   : Dwi Nur Septiati")
+print("NIM    : 312110215")
+print("Matkul : Bahasa Pemograman")
+
+print("#######")
+print("#######")
+
+#Pembuka
+print("Assalamualaikum")
+print("Let me introduce my self")
+
+#Nama
+data = input("My name is: ")
+print("Nama lengkap = ",data,)
+
+#Nama panggilan
+data = input("But you call me: ")
+print("Nama Panggilan = ",data,)
+
+#NPM
+data = input("My NPM is: ")
+print("npm = ",data,)
+
+#Tempat lahir
+data = input("I was born in: ")
+print("Tempat lahir = ",data,)
+
+#Umur
+data_int = int(input("umur: "))
+print("umur = ",data_int, "years old")
+
+#Alamat
+data = input("iam very glad if you want to invite my house in: ")
+print("Alamat = ",data,)
+
+#Telepon
+data_int = int(input("so, dont forget to call me before with the number: "))
+print("telepon = ",data,)
